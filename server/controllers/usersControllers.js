7414931f7642df81a44d34a789fe4862a73b4fb0
Jhonni10 <jhonieripianoscue@gmail.com>
2023-12-13 16:41:57 -0500

    componentes new setAvatar

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -58,3 +58,26 @@ module.exports.login = async ( req , res , next) => {
         next(error.message);
     }
 };
+
+
+
+module.exports.setAvatar = async (req, res, next) => {
+    try {
+      const userId = req.params.id;
+      const avatarImage = req.body.image;
+      const userData = await User.findByIdAndUpdate(
+        userId,
+        {
+          isAvatarImageSet: true,
+          avatarImage,
+        },
+        { new: true }
+      );
+      return res.json({
+        isSet: userData.isAvatarImageSet,
+        image: userData.avatarImage,
+      });
+    } catch (ex) {
+      next(ex);
+    }
+  };
